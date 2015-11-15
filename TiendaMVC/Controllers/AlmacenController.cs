e88c7c30b7b5bbbf13b22cede8d9afd64ecc32cb
Cristina Machuca Aparicio <Cristina.Machuca@tajamar365.com>
2015-11-15 18:08:42 +0100

    4

---
+++ TiendaMVC/Controllers/AlmacenController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaMVC.Models;
+
+namespace TiendaMVC.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        private Tienda10Entities db = new Tienda10Entities();
+
+        // GET: Almacen
+        public ActionResult Index()
+        {
+            var data = db.Almacen;
+            return View(data);
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            var data = db.Almacen.Find(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Modificar(Almacen model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Borrar(int id)
+        {
+            var data = db.Almacen.Find(id);
+
+            if (data.ProductoAlmacen.Any())
+            {
+                db.ProductoAlmacen.RemoveRange(data.ProductoAlmacen);
+                
+            }
+
+            db.Almacen.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TiendaMVC/Controllers/ProductoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaMVC.Models;
+
+namespace TiendaMVC.Controllers
+{
+    public class ProductoController : Controller
+    {
+        Tienda10Entities db = new Tienda10Entities();
+        // GET: Producto
+        public ActionResult Index()
+        {
+            var data = db.Producto;
+            return View(data);
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            var data = db.Producto.Find(id);
+            return View(data);
+
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Producto());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Producto model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Producto.Add(model);
+                db.SaveChanges();
+                return View(new Producto());
+            }
+
+            return View(model); //Si los datos introducidos no son correctos, los vuelve a cargar en el formulario para que se modifiquen.
+        }
+   }
+}
